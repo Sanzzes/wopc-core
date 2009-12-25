bafed4d5fb8349f01137231838049879068ef804
Sanzzes <stevenbohm@sanzzes-city.com>
2009-12-25 14:55:14 +0100

    [2299] - Damage Calculation 3.3.0

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,8 +1,8 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "2298"
- #define FILEVER        0,0,2298,0
- #define PRODUCTVER     0,0,2298,0
- #define STRFILEVER     "0, 0, 2298"
- #define STRPRODUCTVER  "0, 0, 2298"
+ #define REVISION_NR "2299"
+ #define FILEVER        0,0,2299,0
+ #define PRODUCTVER     0,0,2299,0
+ #define STRFILEVER     "0, 0, 2299"
+ #define STRPRODUCTVER  "0, 0, 2299"
 #endif // __REVISION_NR_H__
